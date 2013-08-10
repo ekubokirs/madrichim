8c608ca53b35476d60b51127ce6c4a2e3ece3516
Emilia Kubo Kirschenbaum <ekubokirs@gmail.com>
2013-08-09 17:08:01 -0700

    set up rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Madrichim::Application.config.secret_key_base = 'ce8667c88309b39eb8a4b55aa93bd24ad5454b0bfb2480e40c0fee5434da2efb6317a25c7f5bca21efc1f0bad02397f66edf38703ae18490c2810e55a99d5fd8'
