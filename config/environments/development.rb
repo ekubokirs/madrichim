58e9f4420d6c212557aecb6721e1e41f698ec6ac
Emilia Kubo Kirschenbaum <ekubokirs@gmail.com>
2013-08-20 19:26:57 -0700

    attempt at deployment 3:mailers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,9 +13,23 @@ Madrichim::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+   # Configure to send mail with gmail account
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'ctjmb.org',
+      user_name:            'emilia',
+      password:             '2404john',
+      authentication:       'plain',
+      enable_starttls_auto: true
+  }
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+
+  # Don't care if the mailer can't send./mailcatcher
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
   
